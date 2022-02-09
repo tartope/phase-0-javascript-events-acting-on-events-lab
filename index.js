797d552c253656c50bd4e15b98a6118d10cc82a5
Tunisia Artope <artope@yahoo.com>
2022-02-08 20:12:28 -0500

    commit tree 0de1479b1e8e6ebffe55b0579c01474e54b79f0b

--- index.js
+++ index.js
@@ -1,32 +1,86 @@
 // Your code here
+//1. get and create variable for dodger id.
 const dodger = document.getElementById("dodger");
-
+//2. use (document.addEventListener("keydown", function (event) {console.log(event);});) to determine key property ('ArrowLeft/ArrowRight') for keys.
+//3. create function moveDodgerLeft.
+//3. use (dodger.style.left) to get the current value of the dodgers property and use the 'replace()' method (replace 'px' with empty string) to strip out the 'px', then store the result in 'leftNumbers' variable.
+//4. parseInt leftNumbers and store in 'left' variable.
+//5. make an if statement to stop dodger from leaving the edge of the game field, and update the dodger's style.left property using string interpolation, injecting the current value minus 1.
 function moveDodgerLeft() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
-  
-    if (left > 0) {
-      dodger.style.left = `${left - 1}px`;
-    }
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
   }
-
-  document.addEventListener("keydown", function (e) {
-    if (e.key === "ArrowLeft") {
-      moveDodgerLeft();
-    }
-  });
-
-function moveDodgerRight() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
-
-    if (left < 360) {
-        dodger.style.left = `${left + 1}px`;
-    }
+}
+//6. make eventListener for moveDodgerLeft/Right with if statement to return functions moveDodgerLeft/Right.
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }else if(e.key === "ArrowRight"){
+    moveDodgerRight();
+  }
+});
+//7. make moveDodgerRight funcion using steps above; instead change if statement to define right boarder of game field (game field size (400px) - dodger size (40px)).
+//8. make string interpolation left + 1.
+function moveDodgerRight(){
+  const leftNumbers = dodger.style.left. replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+  if(left < 360){
+    dodger.style.left = `${left + 1}px`;
+  }
 }
 
-document.addEventListener("keydown", function (e) {
-    if (e.key === "ArrowRight") {
-        moveDodgerRight();
-    }
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const dodger = document.getElementById("dodger");
+
+// function moveDodgerLeft() {
+//     const leftNumbers = dodger.style.left.replace("px", "");
+//     const left = parseInt(leftNumbers, 10);
+  
+//     if (left > 0) {
+//       dodger.style.left = `${left - 1}px`;
+//     }
+//   }
+
+//   document.addEventListener("keydown", function (e) {
+//     if (e.key === "ArrowLeft") {
+//       moveDodgerLeft();
+//     }
+//   });
+
+// function moveDodgerRight() {
+//     const leftNumbers = dodger.style.left.replace("px", "");
+//     const left = parseInt(leftNumbers, 10);
+
+//     if (left < 360) {
+//         dodger.style.left = `${left + 1}px`;
+//     }
+// }
+
+// document.addEventListener("keydown", function (e) {
+//     if (e.key === "ArrowRight") {
+//         moveDodgerRight();
+//     }
+// });
